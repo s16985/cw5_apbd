f7da1d3d64daea2efee446c318c5adc81d82034c
pablo230390 <pawelwawrzenczyk@gmail.com>
2020-05-20 14:06:48 +0200

    Rozwiązania zadań 5

---
+++ WebApplication1/WebApplication1/Services/IStudentsDbService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.DTOs.Requests;
+using WebApplication1.DTOs.Responses;
+
+namespace WebApplication1.Services
+{
+	public interface IStudentsDbService
+	{
+		EnrollStudentResponse EnrollStudent(EnrollStudentRequest request);
+		PromoteStudentsResponse PromoteStudents(PromoteStudentsRequest request);
+	}
+}
